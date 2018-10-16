dc74d6cec0814b3d2554acf21ae26b5101b07b5d
skydavis435 <dmayer2@gmail.com>
2018-10-15 22:17:48 -0500

    added aggregate calculations

--- R/agg.bstar.R
+++ R/agg.bstar.R
@@ -6,5 +6,12 @@
 #Ensure filters are in place
   sub.data<-subset(data, Detections.qty > 1)
   sub.data<-subset(data, AverageAltitude.ft > 1000)  
-#Aggregate by GMT day  
+#Aggregate by GMT day
+  sub.data$Start.Time_US.Central<-as.POSIXct(x = sub.data$Start.Time_US.Central,format="%Y-%m-%d %H:%M:%S",tz = "US/Central")
+  sub.data$Date.Time.GMT <- format(sub.data$Start.Time_US.Central,tz = "GMT")
+  sub.data$GMT.Date<-as.Date(sub.data$Date.Time.GMT)
+  sub.data$long.id<-paste0(sub.data$Track, sub.data$RadarTrackNumber)
+  
+  Track.Count<-setNames(aggregate(sub.data$long.id ~ sub.data$GMT.Date, FUN=length),c("GMT.Date","Track.Count"))
+  Avg.Speed<-setNames(aggregate(sub.data$AverageVelocity..mph ~ sub.data$GMT.Date, FUN=mean),c("GMT.Date","Avg.Velocity.MPH"))
   
