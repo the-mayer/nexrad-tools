82ed9285ce588cc59cac058c4ce443dadacbd211
David Mayer <dmayer2@gmail.com>
2020-12-05 22:31:31 -0700

    modify parameters - specify input file path - specify output file directory - calculate output filename, based on input file

--- R/create_netCDF.R
+++ R/create_netCDF.R
@@ -1,15 +1,17 @@
 #' Create NetCDF
 #'
-#' @param in_file_path Path to NEXRAD Data file
-#' @param out_file_path Path to output file
+#' @param in_file Path to NEXRAD Data file
+#' @param out_path Path to output directory
 #'
 #' @return
 #' @export
 #'
+#' @importFrom glue glue
 #' @importFrom rJava .jaddClassPath .jcall .jnew 
-create_netCDF <- function(in_file_path, out_file_path) {
+create_netCDF <- function(in_file, out_file_path) {
   .jaddClassPath('inst/java/BlockBunzipper.jar')
   .jaddClassPath('inst/java/commons-compress-1.20.jar')
   ncdf_writer <- .jnew('BlockBunzipper')
-  .jcall(ncdf_writer, returnSig="V", method="main", c(in_file_path, out_file_path) )
+  out_file <- glue::glue('{out_path}/{basename(in_file)}.ncdf')
+  .jcall(ncdf_writer, returnSig="V", method="main", c(in_file, out_file) )
 }
