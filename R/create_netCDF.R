e94fc74f736c23ce57776cd3c1efcf04f4a40a05
David Mayer <dmayer2@gmail.com>
2020-12-06 18:01:13 -0700

    modify create_netCDF - use correct extension for output file - place output file in the same directory as input - catch and recover from Java IOExceptions, when FileWriter2 is unable to determine the source CDM - remove decompressed intermediary file, if exists

--- R/create_netCDF.R
+++ R/create_netCDF.R
@@ -1,17 +1,25 @@
 #' Create NetCDF
 #'
 #' @param in_file Path to NEXRAD Data file
-#' @param out_path Path to output directory
 #'
 #' @return
 #' @export
 #'
 #' @importFrom glue glue
 #' @importFrom rJava .jaddClassPath .jcall J
-create_netCDF <- function(in_file, out_file_path) {
+create_netCDF <- function(in_file) {
   .jaddClassPath('inst/java/toolsUI-5.3.2.jar')
   ncdf_writer <- J('ucar.nc2.FileWriter2')
-  out_file <- glue::glue('{out_path}/{basename(in_file)}.ncdf')
-  .jcall(ncdf_writer, returnSig="V", method="main", c('-in', in_file, '-out', out_file) )
-  try(file.remove(glue::glue('{in_file}.uncompress')), silent = T)
-}
+  out_file <- glue::glue('{in_file}.nc')
+  tryCatch({
+    if(!file.exists(out_file) ) {
+      ncdf_writer$main(c('-in', in_file, '-out', out_file))
+      if(file.exists(glue::glue('{in_file}.uncompress')) ) {
+        file.remove(glue::glue('{in_file}.uncompress'))
+        }
+      }
+    }, 
+    IOException = function(e){
+      e$message
+      })
+  }
